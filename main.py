c4aef8aa5ad088b0d973412822df23ce216b0cc2
Mamata <100906354+mamata171@users.noreply.github.com>
2022-03-12 17:24:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+print('************BIOGRAPGH***********')
+
+name=input('Name          :')
+mob_no=int(input('Mobile_no     :'))   
+clr=input('favorite color :')
+mov=input('favorite movie :')
+foo=input('favorite food :')
+cel=input('favorite celebrity:')
+spo=input('favorite sports:')
+song=input('favorite song :')
+place=input('favoritr place:')
+    
+print('Select any one of two choice')
+print('1)Coffe/Tea :',end="")
+x=input('')
+print('2)Day/Night :',end="")
+y=input('')
+print('3)shop/long_drive :',end="")
+z=input('')
+print('4)beach/mountain :',end="")
+u=input('')
+
+print('************BIOGRAOGH***********')
+
+print('Name       :',name)
+print('mobile_num :', mob_no)
+print('***Favorites***')
+print('color      :',clr)
+print('movie      :',mov)
+print('song       :',song)
+print('food       :',foo)
+print('celebrity  :',cel)
+print('place      :',place)
+print('sports      :',spo)
+if x=='coffe':
+    print('a coffe lover')
+else:
+    print('love to chaiye never ends')
+
+if y=='day':
+    print('day will be day')
+else:
+    print('that cold wind ,moon stars a night is best tha day')
+
+if z=='shop':
+    print('shopping is my craze')
+else:
+    print('wish to go long drive with my bf')
+
+if u=='beach':
+    print('like beach much,when waves comes woww')
+else:
+    print('wish to go once to mountain')
+
+print('one word about me ')
+print('lets make it as fun,select any number from 1 to 5 :',end="")
+a=int(input(''))
+if a==1:
+    print('i love you')
+if a==2:
+    print('i hate you')
+if a==3:
+    print('u r stupid')
+if a==4:
+    print('u r so sweet')
+if a==5:
+    print('my bestii')
+    
+
+
+
+
+
